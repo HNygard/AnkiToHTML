7fc3a744c16588f002b4472069431520621792df
Hallvard Nygard <hallvard.nygard@gmail.com>
2010-12-08 09:56:30 +0100

    Fixing layout with tables and CSS

--- index.php
+++ index.php
@@ -17,27 +17,64 @@ if(!isset($_GET['file']) && $h = opendir($directory_ankifiles))
 	exit;
 }
 
+echo '<style>
+
+table { border-collapse:collapse; }
+
+.line
+{
+	height: 30px;
+}
+
+.question
+{
+	font-size: 1.5em;
+	border: 1px dashed gray;	
+}
+
+.answer
+{
+	font-size: 1.3em;
+	left: 50px;
+	border: 1px dashed gray;
+}
+
+.tags
+{
+	font-size: 1em;
+	color: gray;
+	border: 1px dashed gray;
+}
+
+</style>';
+
 $file = str_replace('..', '', $_GET['file']);
 $file = str_replace('/',  '', $file);
 $file = str_replace('\\', '', $file);
 
 $content = file_get_contents($directory_ankifiles.'/'.$file);
 
+echo '<table>'.chr(10).chr(10);
 $lines = explode("\n", $content);
 foreach($lines as $line)
 {
-	echo '<div class="line">';
 	$vars = explode("\t", $line);
-	foreach($vars as $i => $var)
-	{
-		if($i == 0)
-			echo '<div class="question">'.$var.'</div>';
-		elseif($i == 1)
-			echo '<div class="answer">'.$var.'</div>';
-		elseif($i == 2)
-			echo '<div class="tags">'.$var.'</div>';
-		else
-			echo '<div>'.$var.'</div>';
-	}
-	echo '</div>'.chr(10);
+	
+	
+	echo '<tr><td class="question">';
+	if(isset($vars[0]))
+		echo $vars[0];
+	else
+		echo '&nbsp;';
+	echo '</td></tr><tr><td class="answer">';
+	if(isset($vars[1]))
+		echo $vars[1];
+	else
+		echo '&nbsp;';
+	echo '</td></tr>';
+	if(isset($vars[2]) && !empty($vars[2]))
+		echo '<tr><td class="tags">'.$vars[2].'</td></tr>';
+	
+	echo '<tr class="line">&nbsp;</tr>'.chr(10);
 }
+echo '</table>';
